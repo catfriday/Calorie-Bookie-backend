e2388284c97d1fd8b2f2a04d7c6d80d604178d04
Catrina Friday <cat.friday@gmail.com>
2020-10-20 20:08:43 -0400

    fixed methods in bets model and added them to controller

--- app/controllers/api/v1/bets_controller.rb
+++ app/controllers/api/v1/bets_controller.rb
@@ -2,19 +2,19 @@ class Api::V1::BetsController < ApplicationController
 
     def index
         bets = Bet.all
-        render json: bets, except: [:created_at, :updated_at], methods: [:win_or_lose, :reserve]
+        render json: bets, except: [:created_at, :updated_at], methods: [:win_or_lose, :reserve, :days_left]
     end
 
     def show
         bet = Bet.find_by(id: params[:id])
-        render json: bet, except: [:created_at, :updated_at], methods: [:win_or_lose, :reserve]
+        render json: bet, except: [:created_at, :updated_at], methods: [:win_or_lose, :reserve, :days_left]
     end
 
     def create
         user = User.find_by(id: params[:user_id])
         bet = Bet.new(user_id: params[:user_id], amount: params[:amount])
         if bet.save
-            render json: user.bets, except: [:created_at, :updated_at], methods: [:win_or_lose, :reserve]
+            render json: user.bets, except: [:created_at, :updated_at], methods: [:win_or_lose, :reserve, :days_left]
         else
              render json: 'User Not Saved'
         end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -11,11 +11,22 @@ class Bet < ApplicationRecord
 
         elsif user.entries_percentage < 0.90 && user.daily_logs.last.date == Date.yesterday && user.logged >= 0.90
                return "Sorry, not enough entries made durring your 30 day log to win. You forfieted the game"
-        else return "still in the game"
+        else return "Still in the game!"
     end
    end
 
    def reserve
     self.amount * 0.20
+   end
+
+   def days_left
+        user = User.find_by(id: self.user_id)
+        last_date = user.daily_logs.last.date
+        days = (last_date - Date.today).to_i
+        if days < 0
+            return 0
+        else
+            return days 
+        end
    end
 end
